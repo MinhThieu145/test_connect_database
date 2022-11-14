a3e6c2c5bdeb670f25ef5a42b0221ec13bfd31c8
MinhThieu145 <88282475+MinhThieu145@users.noreply.github.com>
2022-11-14 18:15:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
             # Return result
             query_result = sql_executor(raw_code)
             df = pd.DataFrame(query_result, columns = ['checking_date', 'checking_time', 'hour', 'day_of_week', 'date', 'room','status'])
-            st.dataframe(df, width= 1000)
+            st.code(df, width= 1000)
             
     else:
         st.subheader('About')
